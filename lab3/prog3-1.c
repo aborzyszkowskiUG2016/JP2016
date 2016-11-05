421ee25305e94b5590a20fdadd626fdcd21673ea
amb1717 <amb@inf.ug.edu.pl>
2016-11-05 10:00:35 +0100

    nowe

---
+++ lab3/prog3-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  /*
+    int n1i, n2i, n3i;
+    float n1f, n2f, n3f;
+/*
+    n1i=5+3*8/2-3; //14
+    n2i=2%2+2*2-2/2;//4
+    n3i=2*4*(5+9/2);//72
+    n1f=5+3*8/2-3;//14
+    n2f=2%2+2*2-2.0/2;//4
+    n3f=2*4*(5+9.0/2);//76
+    printf("n1=%d, n2=%d, n3=%d\n\n", n1i, n2i, n3i);
+    printf("n1=%.1f, n2=%.1f, n3=%.1f\n\n", n1f, n2f, n3f);
+*/
+    printf("l1=%d, l2=%d\n\n", 3/2, (-3)/2);
+
+  return 0;
+}
